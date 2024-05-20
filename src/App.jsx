1405774d35538a627bbe8f5633b42c7932af8c2e
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-05-20 18:25:27 +0200

    functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,68 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
+    setNewItem("");
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
   return (
     <>
-    <div className="w-1/2">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="flex flex-col">
           <label htmlFor="item">New item</label>
-          <input type="text" id="item" className="border border-gray-300 p-2 w-1/3"/>
+          <input
+            type="text"
+            id="item"
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            className="border border-gray-300 p-2 w-1/3"
+          />
         </div>
         <button className="border border-gray-300 p-2 rounded-md">Add</button>
       </form>
       <h1>Todo list</h1>
       <ul>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="border border-gray-300 p-2 rounded-md">Delete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="border border-gray-300 p-2 rounded-md">Delete</button>
-        </li>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+              </label>
+              <button className="border border-gray-300 p-2 rounded-md">
+                delete
+              </button>
+            </li>
+          );
+        })}
       </ul>
-    </div>
     </>
   );
-};
+}
